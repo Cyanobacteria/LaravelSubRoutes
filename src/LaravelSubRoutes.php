a56b1b86487060c1cd99f36d735b7be38fc8848d
cyanobacteria <vodomelkor@gmail.com>
2019-05-07 10:32:43 +0800

    change to chained cal

--- src/LaravelSubRoutes.php
+++ src/LaravelSubRoutes.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Cyanobacteria\Route;
+
 use App\Repositories\OauthClientRepository;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
@@ -8,56 +9,209 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
 
-class LaravelSubRoutes extends RouteServiceProvider{
+class LaravelSubRoutes extends RouteServiceProvider
+{
 
     private $configPath;
-    //private $subFolderName;
+    private $prefix;
+    private $subRouteFolderName;
+    private $middlewareType;
+    private $middlewares;
+    private $oauthClients;
+    private $middlewareRules;
+    private $ignoreList;
+    private $noPrefix = false;
+
     public function __construct($params)
     {
-        $this->configPath=$params['configPath'];
+        $this->middlewareType = $params['middlewareType'];
+        $this->setMiddlewareType();
     }
 
-    public function mapSubRoutes($params)
+    public function setNoPrefixTrue()
     {
-        foreach ($this->getSubRouteFileList(['subRouteFolderName'=>$params['subRouteFolderName']]) as $fileCompletePath) {
+        $this->noPrefix = true;
+        return $this;
+    }
+
+    public function setConfigPath($configPath)
+    {
+        $this->configPath = $configPath;
+        return $this;
+    }
+
+    public function setPrefix($prifix)
+    {
+        $this->prefix = $prifix;
+        return $this;
+    }
+
+    public function setSubRouteFolderName($subRouteFolderName)
+    {
+        $this->subRouteFolderName = $subRouteFolderName;
+        return $this;
+    }
+
+    public function setSubRouteFileName($subRouteFileName)
+    {
+        $this->subRouteFileName = $subRouteFileName;
+        return $this;
+    }
+
+    public function setMiddlewares($middleware)
+    {
+        $this->middlewares = $middleware;
+        return $this;
+    }
+
+    public function setIgnoreList($list)
+    {
+        $this->ignoreList = $list;
+        return $this;
+    }
+
+    /*
+     * there are some main logic as follow ...
+     * 1.subRoutefolderName
+     * 2.filesName
+     * 3.
+     *
+     * 哪些是必要值？
+     * 沒有middleware
+     */
+
+    private function anyEmpty($Ary)
+    {
+        foreach ($Ary as $value) if (empty($value)) return true;
+        return false;
+    }
+
+    private function allEmpty($Ary)
+    {
+        foreach ($Ary as $value) if (!empty($value)) return false;
+        return true;
+    }
+
+    private function setMiddlewareType()
+    {
+
+        switch ($this->middlewareType) {
+            case 'funcity':
+                $tableEmpty = empty(Schema::hasTable('oauth_clients'));
+                if ($tableEmpty) {
+                    $this->oauthClients = null;
+                } else {
+                    $ary = OauthClientRepository::getAll();
+                    $newAry = array();
+                    foreach ($ary as $k => $v) $newAry[$v->merchant_code] = $v;
+                    $this->oauthClients = (empty($newAry)) ? null : $newAry;
+                }
+                $this->middlewareRules = $this->getSubRouteMiddlewareRules();//get middleware rule
+                break;
+
+            case 'default':
+
+                break;
+
+            //must has value
+
+        }
+    }
+
+//    private $configPath;
+
+//    private $prefix;
+
+//    private $subRouteFolderName;
+
+
+//    private $middlewareType;
+//    private $oauthClients;
+//    private $middlewareRules;
+
+//    private $middlewares;
+
+
+    public function build()
+    {
+        if (empty($this->subRouteFolderName)) return null;//大批引入必須要有子資料夾
+        foreach ($this->getSubRouteFileList(['subRouteFolderName' => $this->subRouteFolderName]) as $fileCompletePath) {
             $fileName = $this->getFileNameForSubRoutes($fileCompletePath);
 
             $middlewares = $this->getSubRouteMiddleware($fileName);
 
-            $route = Route::prefix($fileName);
+            $route = $this->addPrefix($fileName);
 
             $route = $this->addSubRouteMiddleware([
-                'route'=>$route,
-                'middlewares'=>$middlewares
+                'route' => $route,
+                'middlewares' => $middlewares
             ]);
-
-            $route->namespace($this->namespace)->group($fileCompletePath);
-
+            if (empty($route)) {
+                Route::namespace($this->namespace)->group($fileCompletePath);
+            } else {
+                $route->namespace($this->namespace)->group($fileCompletePath);
+            }
         }
+
+
+    }
+
+    private function addPrefix($fileName)
+    {
+        if ($this->noPrefix) return null;
+        $prefix = (!empty($this->prefix)) ? "{$this->preifx}/$fileName" : $fileName;
+        return Route::prefix($prefix);
+
     }
 
     private function addSubRouteMiddleware($params)
     {
         [$route, $middleware] = [$params['route'], $params['middlewares']];
+        if (empty($route)) return $route;
         if (empty($middleware)) return $route;
-        foreach ($middleware as $mw) $route=$route->middleware($mw);
-        return $route;
-    }
+        return $route->middleware($middleware);
 
+    }
+//    private $middlewareType;
+//    private $oauthClients;
+//    private $middlewareRules;
+
+//    private $middlewares;
     private function getSubRouteMiddleware($fileName)
     {
-        if(empty(Schema::hasTable('oauth_clients'))) return null;
-        $oauthClient = OauthClientRepository::getOneByMerchantCode(['merchant_code'=>$fileName]);
-        $rules = $this->getSubRouteMiddlewareRules();//get middleware rule
-        if (empty($oauthClient)) return null; //if null return null
-        if (empty($rules[$oauthClient->id])) return null; //if null return null
-        return $rules[$oauthClient->id]; //return middlewares
+        /*
+         * 需要兩種實現
+         * 1.自己指定
+         * 2.根據設定還有db
+         */
+        $fromConfig = !$this->anyEmpty([
+            $this->middlewareRules,
+            $this->middlewareType,
+            $this->oauthClients,
+        ]);
+        $set = !empty($this->middlewares);
+        $noMiddleware = (empty($fromConfig) and empty($set)) ? true : false;
+        $tableEmpty = empty(Schema::hasTable('oauth_clients'));
+        if ($noMiddleware) {
+            return null;
+        } elseif ($tableEmpty and !$set) {
+            return null;
+        } elseif ($fromConfig) {
+            $rules = $this->getSubRouteMiddlewareRules();//get middleware rule
+            if (empty($this->oauthClients)) return null; //if null return null
+            $oauthClientId=$this->oauthClients[$fileName];
+            if (empty($rules[$oauthClientId])) return null; //if null return null
+            return $rules[$oauthClientId]; //return middlewares
+        } elseif ($set) {
+            return $set;
+        }
+
     }
 
     private function getSubRouteMiddlewareRules()
     {
         // config
-        $merchantsMiddleware = config($this->configPath.'.middlewares');
+        $merchantsMiddleware = config($this->configPath . '.middlewares');
         return $merchantsMiddleware;
 
     }
@@ -75,10 +229,6 @@ class LaravelSubRoutes extends RouteServiceProvider{
         $prefix = explode('.', $prefix);
         return array_shift($prefix);
     }
-
-
-
-
 
 
 }
